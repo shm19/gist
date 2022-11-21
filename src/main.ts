d486233f3568f7dba7d468a22fed3e922c8c89c1
shm <malekishayan79@gmail.com>
2022-11-21 13:04:48 +0330

    feat(filter): add global filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpErrorFilter } from './filters/http-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpErrorFilter(new Logger()));
   await app.listen(3000);
 }
 bootstrap();
